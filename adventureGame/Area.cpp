a8d91f999ae45dd6f7bc288565ebc2aa0f83237d
lam12602 <79139004+lam12602@users.noreply.github.com>
2022-10-21 09:02:03 +0100

    added functions

--- adventureGame/Area.cpp
+++ adventureGame/Area.cpp
@@ -1,1 +1,19 @@
 #include "Area.h"
+#include<string>
+#include<vector>
+
+Area::Area()
+	:name("")
+	, description("")
+	, contents()
+	, exits()
+{
+}
+
+void Area::look(Area description)
+{
+}
+
+void Area::go(Area name)
+{
+}
--- adventureGame/Area.h
+++ adventureGame/Area.h
@@ -1,9 +1,16 @@
 #pragma once
 #include <string>
+#include<vector>
 
 class Area
 {
 public:
+
+	Area();
+	void look(Area description);
+	void go(Area name);
+
+
 	std::string name;
 	std::string description;
 	std::vector<std::string> contents;
